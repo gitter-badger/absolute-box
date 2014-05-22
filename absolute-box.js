7ea480aa9f9f00495dc5b1533edf0d1bcae0f1a0
dcdeiv <davide.dicriscito@gmail.com>
2014-05-22 18:11:00 +0200

    v 3.0.3
    File authenticated and corrected through JSHint. Adapted to Wordpress
    coding strandards.

--- absolute-box.js
+++ absolute-box.js
@@ -1,41 +1,50 @@
-//ABSOLUTE-BOX 3.0.2 by DCDEIV: www.github.com/dcdeiv
+//ABSOLUTE-BOX 3.0.3 by DcDev: http://dcdev.altervista.org
 (function ( $ ) {
     var SRCminimizer = 'minimize@2x.png';
-    var minimizerWidth = '50px'
-    var minimizerHeight = 'auto'
-    
-    $(document).ready(function(){
-        var b = $('body');
-        b.append('<div id="absolute-box-widget">');
-        var box = $('#absolute-box-widget');
-        box.append('<div>');
-        var box1c = box.children(':first-child');
-        box1c.append('<div id="quit-absolute-box-widget">', '<figure id="relative-picfigbox-absolute-widget"></figure>');
-        var quitBox = $('#quit-absolute-box-widget');
-        quitBox.append('<img src="' + SRCminimizer + '" width="' + minimizerWidth + ' height="' + minimizerHeight + ' alt="minimize" />');
-        var figBox = $('#relative-picfigbox-absolute-widget');
-        figBox.append('<img id="fullscreen-absolute-box-widget" src=""/>');
-        var picFigBox = $('#fullscreen-absolute-box-widget');        
+    var minimizerWidth = '50px';
+    var minimizerHeight = 'auto';
+  
+    $( document ).ready(function() {
+        var w           = $( window ).width();
+        var h           = $( window ).height();
+        var h2          = h*0.5;
+        var wAr         = w/h;
         
-        var w = $(window).width();
-        var h = $(window).height();
-        var h2 = h*0.5;
-        var wAr = w/h;
+        $(  'body' ).append( '<div id="absolute-box-widget">' );
+        
+        var box         = $( '#absolute-box-widget' );
+        box.append( '<div>' );
+        
+        var box1c       = box.children( ':first-child' );
+        box1c.append(
+        '<div id="quit-absolute-box-widget">',
+        '<figure id="relative-picfigbox-absolute-widget"></figure>'
+        );
+        
+        var quitBox     = $( '#quit-absolute-box-widget' );
+        quitBox.append(
+        '<img src="' + SRCminimizer + '" width="' + minimizerWidth +
+        ' height="' + minimizerHeight + ' alt="minimize" />'
+        );
+        
+        var figBox      = $( '#relative-picfigbox-absolute-widget' );
+        figBox.append( '<img id="fullscreen-absolute-box-widget" src=""/>' );
+        
+        var picFigBox   = $( '#fullscreen-absolute-box-widget' );
                 
-        $.fn.absoluteBoxWidgetStyles = function( options ){
-            
+        $.fn.absoluteBoxWidgetStyles = function( options ) {
             var defaults = {
                  boxStyle: {
-                     zIndex: 2000000,
                      position: 'fixed',
                      top: 0,
                      right: 0,
                      bottom: 0,
                      left: 0,
-                     backgroundColor: '#000',
+                     zIndex: 2000000,
+                     backgroundColor: '#000'
                  },
                  box1cStyle: {
-                     position: 'relative',
+                     position: 'relative'
                  },
                  quitBoxStyle: {
                      position: 'absolute',
@@ -46,201 +55,192 @@
                      zIndex: 300000000,
                      width: minimizerWidth,
                      height: minimizerHeight,
-                     backgroundColor: 'rgba(0,0,0,0.5)',
-                     color: '#fff',
-                     webkitBorderRadius: '0.5em',
-                     mozBorderRadius: '0.5em',
+                     '-webkit-border-radius': '0.5em',
+                     '-moz-border-radius': '0.5em',
                      borderRadius: '0.5em',
+                     backgroundColor: 'rgba(0,0,0,0.5)',
+                     color: '#fff'
                  },
                  figBoxStyle: {
-                    position: "relative",
-                    maxWidth: "100%",
-                    maxHeight: "100%",
-                    width: "100%",
-                    height: "100%",
-                    textAlign: "center",
+                     position: 'relative',
+                     width: '100%',
+                     maxWidth: '100%',
+                     height: '100%',
+                     maxHeight: '100%',
+                     textAlign: 'center'
                  },
                  picFigBoxStyle: {
-                    position: "absolute",
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
+                     position: "absolute",
+                     top: 0,
+                     right: 0,
+                     bottom: 0,
+                     left: 0
                  },
             };
             
             var settings = $.extend( true, {}, defaults, options );
             
-            return this.each(function(){
+            return this.each(function() {
                 
-                $(document).ready(function(){
+                $( document ).ready(function() {
                     box.hide();
-                    
-                    box.css(settings.boxStyle);
-                    box1c.css(settings.box1cStyle);
-                    quitBox.css(settings.quitBoxStyle);
-                    figBox.css(settings.figBoxStyle);
-                    picFigBox.css(settings.picFigBoxStyle);
+                    box.css( settings.boxStyle );
+                    box1c.css( settings.box1cStyle );
+                    quitBox.css( settings.quitBoxStyle );
+                    figBox.css( settings.figBoxStyle );
+                    picFigBox.css( settings.picFigBoxStyle );
                 });
-                
             });
         };
         
-        $('body').absoluteBoxWidgetStyles();
+        $(  'body' ).absoluteBoxWidgetStyles();
         
         $.fn.absoluteBoxWidget = function() {
             this.each(function() {
                 var pic = $( this );
+                
                 pic.click(function(){
-                    var wP = pic.width();
-                    var hP = pic.height();
-                    var pAr = wP/hP;
-                    var nWp = h/pAr;
-                    var nWp2 = nWp*0.5;
-                    var nHp = w/pAr;
-                    var nHp2 = nHp*0.5;
-                    var srcP = pic.attr('src');
+                    var wP      = pic.width();
+                    var hP      = pic.height();
+                    var pAr     = wP/hP;
+                    var nHp     = w/pAr;
+                    var nHp2    = nHp*0.5;
+                    var srcP    = pic.attr( 'src' );
                     
-                    picFigBox.attr('src', srcP);
+                    picFigBox.attr( 'src', srcP );
                     
-                    if (pAr < wAr) {
+                    if ( pAr < wAr ) {
                         picFigBox.css({
-                            "height": h,
-                            "width": "auto",
-                            "position": "relative",
-                            "top": "",
-                            "margin-top": "",
+                            position: 'relative',
+                            top: '',
+                            marginTop: '',
+                            width: 'auto',
+                            height: h
                         });
                     } else {
                         picFigBox.css({
-                            "height": "auto",
-                            "width": "100%",
-                            "position": "absolute",
-                            "top": h2,
-                            "margin-top": -nHp2,
+                            position: 'absolute',
+                            top: h2,
+                            marginTop: -nHp2,
+                            width: '100%',
+                            height: 'auto'
                         });                            
                     }
                     
-                    b.css({
-                        "overflow-y": "hidden",
+                    $(  'body' ).css({
+                        'overflow-y': 'hidden'
                     });
                     
                     box.show();
                     
-                    $(window).resize(function(){
-                        var w = $(window).width();
-                        var h = $(window).height();
-                        var h2 = h*0.5;
-                        var wAr = w/h;
-                        var nWp = h/pAr;
-                        var nWp2 = nWp*0.5;
-                        var nHp = w/pAr;
-                        var nHp2 = nHp*0.5;
+                    $( window ).resize(function(){
+                        var w       = $( window ).width();
+                        var h       = $( window ).height();
+                        var h2      = h*0.5;
+                        var wAr     = w/h;
+                        var nHp     = w/pAr;
+                        var nHp2    = nHp*0.5;
                         
-                        if (pAr < wAr) {
+                        if ( pAr < wAr ) {
                             picFigBox.css({
-                                "height": h,
-                                "width": "auto",
-                                "position": "relative",
-                                "top": "",
-                                "margin-top": "",
+                                position: 'relative',
+                                top: '',
+                                marginTop: '',
+                                width: 'auto',
+                                height: h
                             });
                         } else {
                             picFigBox.css({
-                                "height": "auto",
-                                "width": "100%",
-                                "position": "absolute",
-                                "top": h2,
-                                "margin-top": -nHp2,
+                                position: 'absolute',
+                                top: h2,
+                                marginTop: -nHp2,
+                                width: '100%',
+                                height: 'auto'
                             });                            
                         }                        
                     });
                                              
-                    quitBox.click(function(){
+                    quitBox.click(function() {
                         box.hide();
-                        b.show();
-                        b.css({
-                            "overflow-y": "",
+                        $(  'body' ).show();
+                        $(  'body' ).css({
+                            'overflow-y': ''
                         });
                     });
                 });
  
-                $(window).resize(function(){
-                    var w = $(window).width();
-                    var h = $(window).height();
-                    var h2 = h*0.5;
+                $( window ).resize(function(){
+                    var w   = $( window ).width();
+                    var h   = $( window ).height();
+                    var h2  = h*0.5;
                     var wAr = w/h;
                             
                     pic.click(function(){
-                        var wP = pic.width();
-                        var hP = pic.height();
-                        var pAr = wP/hP;
-                        var nWp = h/pAr;
-                        var nWp2 = nWp*0.5;
-                        var nHp = w/pAr;
-                        var nHp2 = nHp*0.5;
-                        var srcP = pic.attr('src');
+                        var wP      = pic.width();
+                        var hP      = pic.height();
+                        var pAr     = wP/hP;
+                        var nHp     = w/pAr;
+                        var nHp2    = nHp*0.5;
+                        var srcP    = pic.attr( 'src' );
                         
-                        picFigBox.attr('src', srcP);
+                        picFigBox.attr( 'src', srcP );
                         
-                        if (pAr < wAr) {
+                        if ( pAr < wAr ) {
                             picFigBox.css({
-                                "height": h,
-                                "width": "auto",
-                                "position": "relative",
-                                "top": "",
-                                "margin-top": "",
+                                position: 'relative',
+                                top: '',
+                                marginTop: '',
+                                width: 'auto',
+                                height: h
                             });
                         } else {
                             picFigBox.css({
-                                "height": "auto",
-                                "width": "100%",
-                                "position": "absolute",
-                                "top": h2,
-                                "margin-top": -nHp2,
+                                position: 'absolute',
+                                top: h2,
+                                marginTop: -nHp2,
+                                width: '100%',
+                                height: 'auto'
                             });                            
                         }
                         
-                        b.css({
-                            "overflow-y": "hidden",
+                        $(  'body' ).css({
+                            'overflow-y': 'hidden'
                         });
                         
                         box.show();
                         
-                        $(window).resize(function(){
-                            var w = $(window).width();
-                            var h = $(window).height();
-                            var h2 = h*0.5;
-                            var wAr = w/h;
-                            var nWp = h/pAr;
-                            var nWp2 = nWp*0.5;
-                            var nHp = w/pAr;
-                            var nHp2 = nHp*0.5;
+                        $( window ).resize(function() {
+                            var w       = $( window ).width();
+                            var h       = $( window ).height();
+                            var h2      = h*0.5;
+                            var wAr     = w/h;
+                            var nHp     = w/pAr;
+                            var nHp2    = nHp*0.5;
                             
-                            if (pAr < wAr) {
+                            if ( pAr < wAr ) {
                                 picFigBox.css({
-                                    "height": h,
-                                    "width": "auto",
-                                    "position": "relative",
-                                    "top": "",
-                                    "margin-top": "",
+                                    position: 'relative',
+                                    top: '',
+                                    marginTop: '',
+                                    width: 'auto',
+                                    height: h
                                 });
                             } else {
                                 picFigBox.css({
-                                    "height": "auto",
-                                    "width": "100%",
-                                    "position": "absolute",
-                                    "top": h2,
-                                    "margin-top": -nHp2,
+                                    position: 'absolute',
+                                    top: h2,
+                                    marginTop: -nHp2,
+                                    width: '100%',
+                                    height: 'auto'
                                 });                            
                             }                        
                         });
                                                  
                         quitBox.click(function(){
                             box.hide();
-                            b.show();
-                            b.css({
-                                "overflow-y": "",
+                            $(  'body' ).show();
+                            $(  'body' ).css({
+                                'overflow-y': ''
                             });
                         });
                     });
